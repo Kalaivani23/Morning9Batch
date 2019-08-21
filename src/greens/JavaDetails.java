1873852a0488426baeb233f610ff4ae8fa0972f5
SwathyArun <swathylakshmia@gmail.com>
2019-08-21 13:29:56 +0530

    add6

--- src/greens/JavaDetails.java
+++ src/greens/JavaDetails.java
@@ -2,22 +2,24 @@ package greens;
 
 import tech.SeleniumDetails;
 
-public class JavaDetails extends SeleniumDetails{
+public class JavaDetails extends SeleniumDetails {
 
 	public void javaSyllabus() {
 		System.out.println("OOPS");
 		System.out.println("a is a");
-		
+
 	}
+
 	public void java() {
-		int a=22;
-		int b=87;
-System.out.println(a*b);
+		int a = 22;
+		int b = 87;
+		System.out.println(a * b);
 	}
+
 	public static void main(String[] args) {
-		JavaDetails c=new JavaDetails();
+		JavaDetails c = new JavaDetails();
 		c.javaSyllabus();
 		c.java();
-		
+
 	}
 }
