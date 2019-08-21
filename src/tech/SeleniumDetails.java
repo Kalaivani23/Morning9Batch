0d4f42b47cf24caf04b6912d018afe9b35eb47bd
VidyaSullia <vidyashreedhara@gmail.com>
2019-08-21 12:59:06 +0530

    new5

--- src/tech/SeleniumDetails.java
+++ src/tech/SeleniumDetails.java
@@ -6,8 +6,13 @@ public class SeleniumDetails {
 		System.out.println("Locators");
 
 	}
+	private void github() {
+		System.out.println("GIT");
+
+	}
 	public static void main(String[] args) {
 		SeleniumDetails s=new SeleniumDetails();
 		s.seleniumSyllabus();
+		s.github();
 	}
 }
