0237ee17a89e2154dd6e08052920ccd3f7493dbb
SwathyArun <swathylakshmia@gmail.com>
2019-08-21 18:49:08 +0530

    ase1

--- src/greens/BranchDetails.java
+++ src/greens/BranchDetails.java
@@ -5,6 +5,7 @@ public class BranchDetails {
 		System.out.println("Kalaivani");
 		int a = 10;
 		int b = 20;
+		System.out.println(a/b);
 		System.out.println(a + b);
 		System.out.println("Hello");
 		System.out.println("Ok123");
