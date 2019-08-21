d671bb79ad93391201a98e559a2d16bf38a27771
SwathyArun <swathylakshmia@gmail.com>
2019-08-21 19:02:20 +0530

    ase112

--- src/greens/BranchDetails.java
+++ src/greens/BranchDetails.java
@@ -4,11 +4,13 @@ public class BranchDetails {
 	public static void main(String[] args) {
 		System.out.println("Kalaivani");
 		int a = 11;
+		int c=55;
 		int b = 20;
 		System.out.println(a/b);
 		System.out.println(a + b);
 		System.out.println("Hello");
 		System.out.println((a+b)-b);
+		System.out.println((a+b)-c);
 		System.out.println("Ok123");
 		System.out.println("hi");
 		System.out.println(b / a);
