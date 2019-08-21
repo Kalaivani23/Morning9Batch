eb0176cb1f54702555e4c5c242b2fecde0648b07
SwathyArun <swathylakshmia@gmail.com>
2019-08-21 13:29:04 +0530

    add5

--- src/greens/JavaDetails.java
+++ src/greens/JavaDetails.java
@@ -9,9 +9,15 @@ public class JavaDetails extends SeleniumDetails{
 		System.out.println("a is a");
 		
 	}
+	public void java() {
+		int a=22;
+		int b=87;
+System.out.println(a*b);
+	}
 	public static void main(String[] args) {
 		JavaDetails c=new JavaDetails();
 		c.javaSyllabus();
+		c.java();
 		
 	}
 }
