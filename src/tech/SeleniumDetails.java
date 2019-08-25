7052e2ac4e336daeb88e87e20479e25b1a1f912b
[mohanakrishnan07] <[mohanakrishnan07@gmail.com]>
2019-08-25 08:39:36 +0530

    25Aug

--- src/tech/SeleniumDetails.java
+++ src/tech/SeleniumDetails.java
@@ -5,14 +5,14 @@ public class SeleniumDetails {
 	void seleniumSyllabus() {
 		System.out.println("Locators");
 		System.out.println("hi hai");
-
+		System.out.println("Mohankrishnan");
 	}
+
 	public static void main(String[] args) {
-		SeleniumDetails s=new SeleniumDetails();
+		SeleniumDetails s = new SeleniumDetails();
 		System.out.println("how are you");
 		s.seleniumSyllabus();
 		System.out.println("hello");
-		
-		
+
 	}
 }
